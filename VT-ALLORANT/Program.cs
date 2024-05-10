23b4355da8863920a1cfc54f289008f51b181587
NekoFerris <nekoandreas@gmail.com>
2024-05-10 14:54:27 +0200

    remove again :D

--- VT-ALLORANT/Program.cs
+++ VT-ALLORANT/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+class Program
+{
+    private DiscordSocketClient _client;
+
+    static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        await RegisterCommandsAsync();
+        await _client.LoginAsync(TokenType.Bot, "your-bot-token");
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        // Register your commands here
+    }
+}
